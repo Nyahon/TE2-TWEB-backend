c5506c4ee27d70a7fbc2c0ba7bc73c6a36711f3d
Etnarion <samuelmayorweb@gmail.com>
2019-01-10 12:33:20 +0100

    cors

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ const corsOptions = {
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors(corsOptions))
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*')
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 	next()
 })
+
+app.use(cors(corsOptions))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const mongoOpt = {
 	useNewUrlParser: true,
